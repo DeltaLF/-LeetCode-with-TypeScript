d27067df2212b05768771bd55a3d62f042b36648
Patrick <patricktsaikl@gmail.com>
2023-02-18 08:03:57 +0000

    226_Invert_Binary_Tree

--- src/leetcode_75/tree/226_Invert_Binary_Tree.ts
+++ src/leetcode_75/tree/226_Invert_Binary_Tree.ts
@@ -6,37 +6,93 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+import { Queue } from "@datastructures-js/queue";
 import { TreeNode } from "./tree.type";
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
  */
- var invertTreeII = function(root:TreeNode|null):TreeNode|null {
-    if(!root) return root
-    const revertHelper = (arr:TreeNode[]):void=>{
-        for(let i=0;i<arr.length;i++){
-            [arr[i].left,arr[i].right] =[arr[i].right, arr[i].left]
-        }
-        return 
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  // try solution without recursion
+  if (!root) return null;
+  const queue = new Queue<TreeNode>();
+  queue.enqueue(root);
+  while (queue.size() > 0) {
+    const node = queue.dequeue()!;
+    [node.left, node.right] = [node.right, node.left];
+    if (node.left) queue.enqueue(node.left);
+    if (node.right) queue.enqueue(node.right);
+  }
+  return root;
+}
+
+function invertTreeSimple(root: TreeNode | null): TreeNode | null {
+  if (!root) return null;
+  [root.left, root.right] = [root.right, root.left];
+  invertTreeSimple(root.left);
+  invertTreeSimple(root.right);
+  return root;
+}
+
+function invertTree20230208(root: TreeNode | null): TreeNode | null {
+  /*
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+       4
+     /   \
+    2     7              
+   / \   /  \
+  1  3  6    9   
+      
+       4
+     /  \
+    7    2
+   9 6  3  1   
+   should I use DFT or BFT to reverse?
+   seems BFT:
+
+*/
+  if (!root) return null;
+  if (root.left || root.right) {
+    const temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+  }
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+}
+
+var invertTreeII = function (root: TreeNode | null): TreeNode | null {
+  if (!root) return root;
+  const revertHelper = (arr: TreeNode[]): void => {
+    for (let i = 0; i < arr.length; i++) {
+      [arr[i].left, arr[i].right] = [arr[i].right, arr[i].left];
+    }
+    return;
+  };
+
+  // Breadth First Traversal
+  let node: TreeNode | null = root;
+  // iterate through with bft
+  let layerArr: TreeNode[] = [];
+  layerArr.push(node);
+  revertHelper(layerArr);
+
+  while (layerArr.length > 0) {
+    // iterate through different layer
+    const newLayerArr: TreeNode[] = [];
+    for (let i = 0; i < layerArr.length; i++) {
+      // iterate through single layer
+      if (!!layerArr[i].left) newLayerArr.push(layerArr[i].left!);
+      if (!!layerArr[i].right) newLayerArr.push(layerArr[i].right!);
     }
 
-    // Breadth First Traversal
-    let node:TreeNode | null = root;
-    // iterate through with bft
-    let layerArr:TreeNode[] = [];
-    layerArr.push(node);
-    revertHelper(layerArr);
-
-    while(layerArr.length > 0){ // iterate through different layer
-        const newLayerArr:TreeNode[] =[]
-        for(let i=0;i<layerArr.length;i++){ // iterate through single layer
-            if(!!layerArr[i].left) newLayerArr.push(layerArr[i].left!);
-            if(!!layerArr[i].right) newLayerArr.push(layerArr[i].right!);
-        }
-
-        revertHelper(newLayerArr);
-        layerArr = newLayerArr;
-        /*
+    revertHelper(newLayerArr);
+    layerArr = newLayerArr;
+    /*
         
         let say we go to layer 3
         // wrong approach: because before we swapped layer 4, layer will be swapped first!
@@ -55,20 +111,18 @@ import { TreeNode } from "./tree.type";
         / \ / \ 
        0  n 6  9 
          */
-    }
+  }
 
-    return root;
+  return root;
 };
 
-
-
-const invertTree= (root:TreeNode|null):TreeNode|null => {
-    if(!root) return root;
-    [root.left,root.right] = [root.right, root.left];
-    if(!!root.left) invertTreeII(root.left);
-    if(!!root.right) invertTreeII(root.right);
-    return root;
-    /*
+const invertTreeOld = (root: TreeNode | null): TreeNode | null => {
+  if (!root) return root;
+  [root.left, root.right] = [root.right, root.left];
+  if (!!root.left) invertTreeOld(root.left);
+  if (!!root.right) invertTreeOld(root.right);
+  return root;
+  /*
     we only need to focus on each ndoe's children
     0      5
           / \ 
@@ -90,9 +144,7 @@ const invertTree= (root:TreeNode|null):TreeNode|null => {
         / \ / \ 
     2  9  6 4  0 
     
-    */    
+    */
+};
 
-}
-
-
-export {invertTree, invertTreeII}
+export { invertTree, invertTreeII };
--- src/leetcode_75/tree/__test__/226_Invert_Binary_Tree.test.ts
+++ src/leetcode_75/tree/__test__/226_Invert_Binary_Tree.test.ts
@@ -1,23 +1,32 @@
-import {invertTree,invertTreeII} from "../226_Invert_Binary_Tree";
-import {TreeNode} from "../tree.type";
+import { invertTree, invertTreeII } from "../226_Invert_Binary_Tree";
+import { TreeNode } from "../tree.type";
 
-it('tests inverTree',()=>{
+describe("test invertTree", () => {
+  it("test", () => {
+    const root = new TreeNode(0);
+    root.left = new TreeNode(1);
+    const reverted = invertTree(root);
+    expect(reverted!.val).toBe(0);
+    expect(reverted!.right!.val).toBe(1);
+  });
+
+  it("tests inverTree", () => {
     // empty
     expect(invertTree(null)).toBeNull();
-    const unReverted =new TreeNode(4)
+    const unReverted = new TreeNode(4);
     let root = new TreeNode(4);
-    unReverted.left = new TreeNode(2)
-    unReverted.right = new TreeNode(7)
-    unReverted.left.left = new TreeNode(1)
-    unReverted.left.right = new TreeNode(3)
-    unReverted.right.left = new TreeNode(6)
-    unReverted.right.right = new TreeNode(9)
-    root.left = new TreeNode(2)
-    root.right = new TreeNode(7)
-    root.left.left = new TreeNode(1)
-    root.left.right = new TreeNode(3)
-    root.right.left = new TreeNode(6)
-    root.right.right = new TreeNode(9)
+    unReverted.left = new TreeNode(2);
+    unReverted.right = new TreeNode(7);
+    unReverted.left.left = new TreeNode(1);
+    unReverted.left.right = new TreeNode(3);
+    unReverted.right.left = new TreeNode(6);
+    unReverted.right.right = new TreeNode(9);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(7);
+    root.left.left = new TreeNode(1);
+    root.left.right = new TreeNode(3);
+    root.right.left = new TreeNode(6);
+    root.right.right = new TreeNode(9);
     invertTree(root);
     expect(root.val).toEqual(unReverted.val);
     expect(root.left.val).toEqual(unReverted.right.val);
@@ -26,6 +35,5 @@ it('tests inverTree',()=>{
     expect(root.left.right.val).toEqual(unReverted.right.left.val);
     expect(root.right.left.val).toEqual(unReverted.left.right.val);
     expect(root.right.right.val).toEqual(unReverted.left.left.val);
-
-
+  });
 });
