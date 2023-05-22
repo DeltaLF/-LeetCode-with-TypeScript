a726683285e53bc7b7724090bc17eb77052db6ff
Patrick <patricktsaikl@gmail.com>
2023-05-22 08:35:52 +0800

    347_Top_K_Frequent_Elements

--- src/leetcode_75/heap/347_Top_K_Frequent_Elements.ts
+++ src/leetcode_75/heap/347_Top_K_Frequent_Elements.ts
@@ -1,42 +1,75 @@
+function topKFrequent(nums: number[], k: number): number[] {
+  /*
+    Input: nums = [1,1,1,2,2,3], k = 2
+    Output: [1,2]
+    Input: nums = [1], k = 1
+    Output: [1]
+     */
+  const map = new Map<number, number>();
+  for (let num of nums) {
+    if (!map.has(num)) {
+      map.set(num, 1);
+    } else {
+      map.set(num, map.get(num)! + 1);
+    }
+  }
+  const mapArr: number[][] = [];
+  for (const [key, value] of map) {
+    mapArr.push([key, value]);
+  }
+  mapArr.sort((a, b) => {
+    return b[1] - a[1];
+  });
+
+  const ans: number[] = [];
+  for (let i = 0; i < k; i++) {
+    ans.push(mapArr[i][0]);
+  }
+  return ans;
+}
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number[]}
  */
- var topKFrequent = function(nums:number[], k:number):number[] {
-    // create a hash map
-    const map:{[key:string]:number} ={};
-    nums.forEach(num=>{
-        const numStr = num.toString();
-        if(!!map[numStr]){
-            map[numStr] += 1;
-        }else{
-            map[numStr] = 1;
-        }
-    });
-
-    let answer:number[]= []
-    // try to optimize with bucket
-    const bucket:number[][] | undefined[] = []
-    for(let key in map){
-        // bucket index = number count (sorted automatically)
-        if( bucket[map[key]] ){
-            bucket[map[key]] = [parseInt(key) as number, ...bucket[map[key]] as number[]];        
-        }else{
-            bucket[map[key]] = [parseInt(key)]
-        }
+var topKFrequentOld = function (nums: number[], k: number): number[] {
+  // create a hash map
+  const map: { [key: string]: number } = {};
+  nums.forEach((num) => {
+    const numStr = num.toString();
+    if (!!map[numStr]) {
+      map[numStr] += 1;
+    } else {
+      map[numStr] = 1;
     }
-    for(let i =bucket.length;i>=0;i--){
-        if(!!bucket[i]){
-            answer = [...answer,...(bucket[i] as number[])];
-        }
-        if(answer.length === k){
-            return answer
-        }
-    }
-    return answer
+  });
 
-    /*
+  let answer: number[] = [];
+  // try to optimize with bucket
+  const bucket: number[][] | undefined[] = [];
+  for (let key in map) {
+    // bucket index = number count (sorted automatically)
+    if (bucket[map[key]]) {
+      bucket[map[key]] = [
+        parseInt(key) as number,
+        ...(bucket[map[key]] as number[]),
+      ];
+    } else {
+      bucket[map[key]] = [parseInt(key)];
+    }
+  }
+  for (let i = bucket.length; i >= 0; i--) {
+    if (!!bucket[i]) {
+      answer = [...answer, ...(bucket[i] as number[])];
+    }
+    if (answer.length === k) {
+      return answer;
+    }
+  }
+  return answer;
+
+  /*
     // brute force iterte k times to find the kth number
     for(let i=0;i<k;i++){
         let topKey:string='';
@@ -53,4 +86,4 @@
     return answer*/
 };
 
-export {topKFrequent}
+export { topKFrequent };
--- src/leetcode_75/heap/__test__/347_Top_K_Frequent_Elemen.test.ts
+++ src/leetcode_75/heap/__test__/347_Top_K_Frequent_Elemen.test.ts
@@ -1,21 +1,26 @@
 import { topKFrequent } from "../347_Top_K_Frequent_Elements";
 
-it('tests topKFrequent',()=>{
-    // one
-    const one = [1]
-    expect(topKFrequent(one,1)).toEqual([1]);
-    // two
-    const two = [1,2]
-    expect(topKFrequent(two,1)).toEqual([2,1]);
-    expect(topKFrequent(two,2)).toEqual([2,1]);
+it("tests topKFrequent", () => {
+  // one
+  const one = [1];
+  expect(topKFrequent(one, 1)).toEqual([1]);
+  // two
+  const two = [1, 2, 1];
+  const ans2_1 = topKFrequent(two, 1);
+  expect(ans2_1).toEqual([1]);
+  const ans2_2 = topKFrequent(two, 2);
+  expect(ans2_2.indexOf(1) >= 0).toBe(true);
+  expect(ans2_2.indexOf(2) >= 0).toBe(true);
 
-    // mutiple
-    const mutiple = [1,1,1,2,2,3]
-    expect(topKFrequent(mutiple,1)).toEqual([1]);
-    expect(topKFrequent(mutiple,2)).toEqual([1,2]);
+  // mutiple
+  const mutiple = [1, 1, 1, 2, 2, 3];
+  expect(topKFrequent(mutiple, 1)).toEqual([1]);
+  const ans_mul_2 = topKFrequent(mutiple, 2);
+  expect(ans_mul_2.indexOf(1) >= 0).toBe(true);
+  expect(ans_mul_2.indexOf(2) >= 0).toBe(true);
+  expect(ans_mul_2.indexOf(3)).toBe(-1);
 
-    // duplicated
-    const dup = [1,1,1,1,1]
-    expect(topKFrequent(dup,1)).toEqual([1]);
-
+  // duplicated
+  const dup = [1, 1, 1, 1, 1];
+  expect(topKFrequent(dup, 1)).toEqual([1]);
 });
